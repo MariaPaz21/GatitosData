ea9a93d1f33b6385977b99e5f0f19cc3cdd5a725
MariaPaz21 <145079814+MariaPaz21@users.noreply.github.com>
2024-03-12 22:02:30 -0500

    fn

--- code/script.js
+++ code/script.js
@@ -1,44 +1,39 @@
-// Archivo: script.js
+fetch('https://meowfacts.herokuapp.com/')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .then(error => console.error('Error fetching users:', error));
 
-// Función para hacer fetch de la API y procesar la respuesta
-function fetchData() {
-    // URL de la API seleccionada
-    const apiUrl = 'https://cat-fact.herokuapp.com';
 
-    // Hacer fetch de la API
-    fetch(apiUrl)
-        .then(response => {
-            // Verificar si la respuesta es exitosa
-            if (!response.ok) {
-                throw new Error('Error al obtener los datos de la API');
-            }
-            // Parsear la respuesta a JSON
-            return response.json();
-        })
-        .then(data => {
-            // Llamar a la función para rellenar la estructura HTML con los datos obtenidos
-            fillHTML(data);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-}
+const newUser = {
+    name: 'Maria Paz',
+    username: 'paz',
+    email: 'pachigavil@gmail.com'
+};
+fetch('https://jsonplaceholder.typicode.com/users', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newUser)
+})
+.then(response => response.json())
+.then(data => console.log('New user created:', data))
+.catch(error => console.error('Error creating user:', error))
 
-// Función para rellenar la estructura HTML con los datos obtenidos de la API
-function fillHTML(data) {
-    // Supongamos que la estructura HTML tiene un elemento con el id "cat-facts"
-    const catFactsElement = document.getElementById('');
-    
-    // Limpiar el contenido previo
-    catFactsElement.innerHTML = '';
+const updatedUserData = {
+    id: 1,
+    name: 'Grazia',
+    username: 'maggie',
+    email: "maggy@gmail.com"
+};
 
-    // Iterar sobre los datos obtenidos y agregarlos a la estructura HTML
-    data.forEach(fact => {
-        const factElement = document.createElement('p');
-        factElement.textContent = fact.text;
-        catFactsElement.appendChild(factElement);
-    });
-}
-
-// Llamar a la función fetchData() cuando el DOM esté completamente cargado
-document.addEventListener('DOMContentLoaded', fetchData);
+fetch('https://jsonplaceholder.typicode.com/users1', {
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(updatedUserData)
+})
+.then(response => response.json())
+.then(data => console.log('User Updated:', data))
+.catch(error => console.error('Error updating user:', error))
